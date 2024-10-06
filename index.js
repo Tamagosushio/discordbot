67502edff84a6e6ce72c5a7ec38e7f5731efdb28
tamagosushio <mitachi.ts75@gmail.com>
2024-10-06 23:25:49 +0900

    [add] pingコマンドを追加

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
 const client = new Client({ intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessages,
 ]});
-const config = require("./config.json");
-const TOKEN = config.token;
+const { token } = require("./config.json");
+
+// ディレクトリ内のファイル名を取得
+const fs = require("fs");
+const commandFiles = fs.readdirSync("./commands")
+                        .filter(file => file.endsWith(".js"))
+client.commands = new Collection();
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
 
 client.on("ready", () => {
     console.log(`${client.user.tag}起動完了`);
-})
+});
 
 client.on("messageCreate", (message) => {
-    console.log(message.content);
-})
+    if(message.author.bot) return;
+    message.channel.send(message.content);
+});
 
-client.login(TOKEN);
+client.on("interactionCreate", async (interaction) => {
+    if(!interaction.isChatInputCommand()) return;
+    const command = interaction.client.commands.get(interaction.commandName);
+    await command.execute(interaction);
+});
+
+client.login(token);
